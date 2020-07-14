f9be24b5e93ca44bf3823f8bf67d5327903d9f02
wangqk <67989969+wangquankun@users.noreply.github.com>
2020-07-15 00:15:59 +0800

    Add files via upload

---
+++ src/support_fun.cpp
@@ -0,0 +1,20 @@
+
+
+#include "support_fun.hpp"
+
+//辅助函数
+
+void SplitString(const string& s, vector<std::string>& v, const string& c) {
+    string::size_type pos1, pos2;
+    pos2 = s.find(c);
+    pos1 = 0;
+    while(string::npos != pos2)
+    {
+        v.push_back(s.substr(pos1, pos2-pos1));
+        
+        pos1 = pos2 + c.size();
+        pos2 = s.find(c, pos1);
+    }
+    if(pos1 != s.length())
+        v.push_back(s.substr(pos1));
+}
