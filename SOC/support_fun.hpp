8c9ce9449f6fe0fff2869f02e8bf71f4a066d596
wangqk <67989969+wangquankun@users.noreply.github.com>
2020-07-14 22:50:02 +0800

    Add files via upload

---
+++ SOC/support_fun.hpp
@@ -0,0 +1,13 @@
+
+
+#ifndef support_fun_hpp
+#define support_fun_hpp
+
+#include <stdio.h>
+#include "all_header.h"
+using namespace std;
+
+//辅助函数声明
+void SplitString(const string& s, vector<std::string>& v, const string& c);
+
+#endif /* support_fun_hpp */
